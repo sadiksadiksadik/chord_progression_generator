3cf58edadecb16de7c23db7d66b452b97ed03813
m-roberts <2947595+m-roberts@users.noreply.github.com>
2022-08-16 21:44:53 +0100

    Big refactor [WIP]

--- src/main.py
+++ src/main.py
@@ -1,120 +1,81 @@
+from dataclasses import dataclass
+from enum import Enum, auto
 import random
 import tkinter as tk
-from chordgen import constants
+
+# import mingus.core.progressions as progressions
+# import mingus.core.scales as scales
 
 
-# Random Chord Generator Functions
-def random_major_progression():
-    # selects a random number of chords/items from the Ionian and assigns string to variable major_progression
-    kinputvar = random.randint(2, 8)
-    major_progression = random.choices(constants.Ionian, k=int(kinputvar))
-
-    # Enables changes to be made to tkinter text boxes
-    progressionbox.config(state="normal")
-
-    # Inserts chord progression string contained in major_progression
-    progressionbox.delete("1.0", "end")
-    progressionbox.tag_configure("center", justify="center")
-    progressionbox.insert("1.0", major_progression, "center")
-
-    # Disables changes for tkinter text boxes
-    progressionbox.config(state="disabled")
+class FunctionType(Enum):
+    SCALE = auto()
+    PROGRESSION = auto()
 
 
-def random_harmonic_minor_progression():
+class Scale(Enum):
+    # Roman numerals represent 1 of 7 chords in a mode
+    IONIAN = ["I", "ii", "iii", "IV", "V", "vi", "viio"]
+    HARMONIC_MINOR = ["i", "iio", "bIII+", "iv", "V", "bVI", "viio"]
+    MELODIC_MINOR = ["i", "ii", "bIII+", "IV", "V", "vio", "viio"]
+    # Interval Training
+    IONIAN_IN_THIRDS = ["I", "iii", "V", "viio", "ii", "IV", "vi"]
 
-    # selects a random number of chords/items from the Ionian and assigns string to variable major_progression
-    kinputvar = random.randint(2, 8)
-    harmonic_minor_progression = random.choices(
-        constants.harmonic_minor, k=int(kinputvar)
-    )
-
-    # Enables changes to be made to tkinter text boxes
-    progressionbox.config(state="normal")
-
-    # Inserts chord progression string contained in major_progression
-    progressionbox.delete("1.0", "end")
-    progressionbox.tag_configure("center", justify="center")
-    progressionbox.insert("1.0", harmonic_minor_progression, "center")
-
-    # Disables changes for tkinter text boxes
-    progressionbox.config(state="disabled")
-
-
-def random_melodic_minor_progression():
-
-    # selects a random number of chords/items from the Ionian and assigns string to variable major_progression
-    kinputvar = random.randint(2, 8)
-    melodic_minor_progression = random.choices(
-        constants.melodic_minor, k=int(kinputvar)
-    )
-
-    # Enables changes to be made to tkinter text boxes
-    progressionbox.config(state="normal")
-
-    # Inserts chord progression string contained in major_progression
-    progressionbox.delete("1.0", "end")
-    progressionbox.tag_configure("center", justify="center")
-    progressionbox.insert("1.0", melodic_minor_progression, "center")
-
-    # Disables changes for tkinter text boxes
-    progressionbox.config(state="disabled")
-
-    # def random_melodic_minor_progression(): MAKE A FUNCTION FOR A RANDOM PROGRESSION
-    # FROM DICT OF CHORDDS  RANDOM PROGRESSION FROM DICT OF CHORDS FROM DIFFERENT MODES!!!!!
-    # def random_melodic_minor_progression():
+    @classmethod
+    def from_menu_option(cls, scale_option: str):
+        scale_option_to_scale = {
+            "Major": Scale.IONIAN,
+            "Minor": Scale.HARMONIC_MINOR,
+            "Melodic": Scale.MELODIC_MINOR,
+            "Ionian in 3rds": Scale.IONIAN_IN_THIRDS,
+        }
+        return scale_option_to_scale[scale_option]
 
 
-# The CHORD GENERATOR BUTTON - Function that Retreives a String from dropdown list and deploys function based on selection but for the button
-def pick_a_func_button_progression():
-
-    selection = scale_menu.get()
-
-    if selection == "Major":
-        random_major_progression()
-    elif selection == "Minor":
-        random_harmonic_minor_progression()
-    elif selection == "Melodic":
-        random_melodic_minor_progression()
+@dataclass
+class ColourPalette:
+    FOREGROUND = "#FFFFF0"
+    BACKGROUND = "#000000"
 
 
-# The SCALE DISPLAY BUTTON -  Function that Retreives a String from dropdown list and displays the notes of the selected Mode
-def pick_a_func_button_scale():
+def update_text_ui(
+    selected_scale_option: str, text_box: tk.Text, function_type: FunctionType
+):
+    """
+    Update UI with a new chord progression.
+    """
 
-    # Retreives Current String Selected on the Dropdown List
-    scale = scale_menu.get()
+    # Enable text changes
+    text_box.config(state="normal")
 
-    # Print Scale Loop
-    if scale == "Major":
-        # Inserts Major Scale into text box
-        progressionbox.config(state="normal")
-        progressionbox.delete("1.0", "end")
-        progressionbox.tag_configure("center", justify="center")
-        progressionbox.insert("1.0", constants.Ionian, "center")
+    # Delete existing text
+    text_box.delete("1.0", "end")
 
-    elif scale == "Minor":
-        # Inserts Harmonic Minor Scale into text box
-        progressionbox.config(state="normal")
-        progressionbox.delete("1.0", "end")
-        progressionbox.tag_configure("center", justify="center")
-        progressionbox.insert("1.0", constants.harmonic_minor, "center")
+    # Center text
+    text_box.tag_configure("center", justify="center")
 
-    elif scale == "Melodic":
-        # Inserts Melodic Minor Scale into text box
-        progressionbox.config(state="normal")
-        progressionbox.delete("1.0", "end")
-        progressionbox.tag_configure("center", justify="center")
-        progressionbox.insert("1.0", constants.melodic_minor, "center")
+    # Determine text
+    selected_scale: list = Scale.from_menu_option(selected_scale_option).value
 
-    elif scale == "Ionian_in_thirds":
-        # Inserts Melodic Minor Scale into text box
-        progressionbox.config(state="normal")
-        progressionbox.delete("1.0", "end")
-        progressionbox.tag_configure("center", justify="center")
-        progressionbox.insert("1.0", constants.Ionian_in_thirds, "center")
+    # Show scale unless we've asked for a progression in the scale
+    text_fields: list = selected_scale
+    if function_type == FunctionType.PROGRESSION:
 
-        # Disables changes for tkinter text boxes
-        progressionbox.config(state="disabled")
+        def get_random_progression_for_scale(chord_scale: list, number_of_results=None):
+            if not number_of_results:
+                number_of_results = random.randint(2, 8)
+
+            major_progression = random.choices(chord_scale, k=number_of_results)
+            return major_progression
+
+        text_fields = get_random_progression_for_scale(selected_scale)
+
+    text: str = " ".join(text_fields)
+
+    # Insert text
+    text_box.insert("1.0", text, "center")
+
+    # Disable text changes
+    text_box.config(state="disabled")
 
 
 def main():
@@ -122,58 +83,62 @@ def main():
     root = tk.Tk()
     root.title("Chord Progression Generator")
     root.geometry("600x450")
-    root.configure(bg="#000000")
+    root.configure(bg=ColourPalette.BACKGROUND)
 
     # Text Box
     progressionbox = tk.Text(root, spacing1=50, height=2, width=180)
-    progressionbox.config(bg="#000000")
-    progressionbox.config(fg="#FFFFF0")
+    progressionbox.config(bg=ColourPalette.BACKGROUND)
+    progressionbox.config(fg=ColourPalette.FOREGROUND)
     progressionbox.config(font=("Terminal", 18, "bold"))
     progressionbox.pack()
 
     # option menu
-    menu_options = ["Major", "Minor", "Melodic", "Ionian_in_thirds"]
-    variable1 = tk.StringVar()
+    menu_options = ["Major", "Minor", "Melodic", "Ionian in 3rds"]
+
     scale_menu = tk.StringVar(root)
     # sets initial name for widget
-    scale_menu.set("Please Choose a Mode")
+    scale_menu.set("Select Mode/Scale")
     dropdownmenu1 = tk.OptionMenu(root, scale_menu, *menu_options)
-    dropdownmenu1.config(bg="#000000")
-    dropdownmenu1.config(fg="#FFFFF0")
+    dropdownmenu1.config(bg=ColourPalette.BACKGROUND)
+    dropdownmenu1.config(fg=ColourPalette.FOREGROUND)
     dropdownmenu1.config(height=1, width=140)
     dropdownmenu1.config(font=("Terminal"))
     dropdownmenu1.pack()
-    dropdownmenu1.config(activebackground="#FFFFF0")
+    dropdownmenu1.config(activebackground=ColourPalette.FOREGROUND)
 
     # Random Chord Progression Generator Button
-    thebutton = tk.Button(
+    submit_button = tk.Button(
         root,
         height=5,
         width=180,
         text="Generate Chord Progression",
         font=("Terminal", 18, "bold"),
-        command=pick_a_func_button_progression,
+        command=lambda: update_text_ui(
+            scale_menu.get(), progressionbox, FunctionType.PROGRESSION
+        ),
     )
-    thebutton.pack(side=tk.TOP)
-    thebutton.configure(bg="#000000")
-    thebutton.configure(fg="#FFFFF0")
-    thebutton.config(activebackground="#FFFFF0")
+    submit_button.pack(side=tk.TOP)
+    submit_button.configure(bg=ColourPalette.BACKGROUND)
+    submit_button.configure(fg=ColourPalette.FOREGROUND)
+    submit_button.config(activebackground=ColourPalette.FOREGROUND)
 
     # Display Notes of Selected Mode Button
-    thebutton = tk.Button(
+    submit_button = tk.Button(
         root,
         height=5,
         width=180,
         text="Generate Notes of Scale",
         font=("Terminal", 18, "bold"),
-        command=pick_a_func_button_scale,
+        command=lambda: update_text_ui(
+            scale_menu.get(), progressionbox, FunctionType.SCALE
+        ),
     )
-    thebutton.pack(side=tk.TOP)
-    thebutton.configure(bg="#000000")
-    thebutton.configure(fg="#FFFFF0")
-    thebutton.config(activebackground="#FFFFF0")
+    submit_button.pack(side=tk.TOP)
+    submit_button.configure(bg=ColourPalette.BACKGROUND)
+    submit_button.configure(fg=ColourPalette.FOREGROUND)
+    submit_button.config(activebackground=ColourPalette.FOREGROUND)
 
-    # Keep display window running
+    # Keep display window running...
     root.mainloop()
 
 
